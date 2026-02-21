f023674dfcfff4781f902f0bf340532771f66c08
Rakshit Panwar <rakshitpanwar03@gmail.com>
2026-02-22 02:50:13 +0530

    commit

--- src/Pages/Transport/Earnings.jsx
+++ src/Pages/Transport/Earnings.jsx
@@ -14,7 +14,7 @@ const Earnings = () => {
             <span className="text-green-600 text-2xl">💰</span>
           </div>
           <h3 className="text-lg font-semibold text-gray-800 mb-2">Earnings Dashboard</h3>
-          <p className="text-gray-600">Your earnings tracking interface will be implemented here.</p>
+          <p className="text-gray-600">We will build an empire of 10000 crores in next 10 to 15 years.</p>
         </div>
       </div>
     </div>
