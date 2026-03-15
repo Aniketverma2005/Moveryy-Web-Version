1942123ff1947589dfd6a3be93f25996e7679dba
Rakshit Panwar <rakshitpanwar03@gmail.com>
2026-03-15 23:27:39 +0530

    commit

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -57,7 +57,7 @@ const Login = () => {
                                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                                 </svg>
                             </div>
-                            <span>Real time tracking and updates</span>
+                            <span>Real-time tracking and updates</span>
                         </div>
                         <div className="flex items-center">
                             <div className="w-6 h-6 bg-white bg-opacity-20 rounded-full flex items-center justify-center mr-4">
